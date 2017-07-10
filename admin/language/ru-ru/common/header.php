11df94f26eb684acc7e2aff17eb67e8f1f4956eb
WhatNot911 <xyngraph@gmail.com>
2017-07-10 20:07:39 +0300

    Translated: header.php

--- admin/language/ru-ru/common/header.php
+++ admin/language/ru-ru/common/header.php
@@ -3,10 +3,10 @@
 $_['heading_title']      = 'OpenCart';
 
 // Text
-$_['text_profile']       = 'Your Profile';
-$_['text_store']         = 'Stores';
-$_['text_help']          = 'Help';
-$_['text_homepage']      = 'OpenCart Homepage';
-$_['text_support']       = 'Support Forum';
-$_['text_documentation'] = 'Documentation';
-$_['text_logout']        = 'Logout';
+$_['text_profile']       = 'Ваш профиль';
+$_['text_store']         = 'Магазины';
+$_['text_help']          = 'Помощь';
+$_['text_homepage']      = 'Домашняя страница OpenCart';
+$_['text_support']       = 'Форум помощи';
+$_['text_documentation'] = 'Документация';
+$_['text_logout']        = 'Выйти';
