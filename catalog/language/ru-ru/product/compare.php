97506119eac662e09dc5e199a6933ce8d665bfc5
WhatNot911 <xyngraph@gmail.com>
2017-07-10 18:00:36 +0300

    OpenCart v3.0.1.2 July 07, 2017 compatible Russian translation. First commit.

---
+++ catalog/language/ru-ru/product/compare.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']     = 'Сравнение товаров';
+
+// Text
+$_['text_product']      = 'Детали товара';
+$_['text_name']         = 'Товар';
+$_['text_image']        = 'Изображение';
+$_['text_price']        = 'Цена';
+$_['text_model']        = 'Модель';
+$_['text_manufacturer'] = 'Брэнл';
+$_['text_availability'] = 'Доступность';
+$_['text_instock']      = 'Есть в наличии';
+$_['text_rating']       = 'Рэйтинг';
+$_['text_reviews']      = 'Основано на %s отзывах(е).';
+$_['text_summary']      = 'Итог';
+$_['text_weight']       = 'Вес';
+$_['text_dimension']    = 'Размер (L x W x H)';
+$_['text_compare']      = 'Сравнение товара (%s)';
+$_['text_success']      = 'Успех: Вы добавили <a href="%s">%s</a> для <a href="%s">сравнения товаров</a>!';
+$_['text_remove']       = 'Успех: Вы изменили параметры сравнения товаров!';
+$_['text_empty']        = 'Вы не выбрали ниодного товара для сравнения.';
---
+++ catalog/language/ru-ru/product/search.php
@@ -0,0 +1,34 @@
+<?php
+// Heading
+$_['heading_title']     = 'Поиск';
+$_['heading_tag']       = 'Тэги - ';
+
+// Text
+$_['text_search']       = 'Товары, подходящие под критерии';
+$_['text_keyword']      = 'Ключевые слова';
+$_['text_category']     = 'Все категории';
+$_['text_sub_category'] = 'Поиск в подкатегориях';
+$_['text_empty']        = 'Нет товаров по данным критериям.';
+$_['text_quantity']     = 'Кол-во:';
+$_['text_manufacturer'] = 'Брэнд:';
+$_['text_model']        = 'Код товара:';
+$_['text_points']       = 'Очки вознаграждения:';
+$_['text_price']        = 'Цена:';
+$_['text_tax']          = 'Без налога:';
+$_['text_reviews']      = 'Основано на %s отзывах.';
+$_['text_compare']      = 'Сравнение товара (%s)';
+$_['text_sort']         = 'Сортировать по:';
+$_['text_default']      = 'Умолчанию';
+$_['text_name_asc']     = 'Названию (А - Я)';
+$_['text_name_desc']    = 'Названию (Я - А)';
+$_['text_price_asc']    = 'Цене (Дешевле &gt; Дороже)';
+$_['text_price_desc']   = 'Цене (Дороже &gt; Дешевле)';
+$_['text_rating_asc']   = 'Рэйтингу (От наименьшего)';
+$_['text_rating_desc']  = 'Рэйтингу (От наибольшего)';
+$_['text_model_asc']    = 'Модэли (А - Я)';
+$_['text_model_desc']   = 'Модэли (Я - А)';
+$_['text_limit']        = 'Показать:';
+
+// Entry
+$_['entry_search']      = 'Критерии поиска';
+$_['entry_description'] = 'Искать в описании товаров';
