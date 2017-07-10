e73351e358794ca1e11025b55b2cdbd78ec3e7e7
WhatNot911 <xyngraph@gmail.com>
2017-07-10 18:16:21 +0300

    More translation.

--- admin/language/ru-ru/common/developer.php
+++ admin/language/ru-ru/common/developer.php
@@ -1,25 +1,25 @@
 <?php
 // Heading
-$_['heading_title']    = 'Developer Settings';
+$_['heading_title']    = 'Настройки разработчика';
 
 // Text
-$_['text_success']     = 'Success: You have modified developer settings!';
-$_['text_theme']       = 'theme';
+$_['text_success']     = 'Успех: Настройки изменены!';
+$_['text_theme']       = 'тема';
 $_['text_sass']        = 'SASS';
-$_['text_cache']       = 'Success: You have cleared the %s cache!';
+$_['text_cache']       = 'Успех: Кэш %s был очищен!';
 
 // Column
-$_['column_component'] = 'Component';
-$_['column_action']    = 'Action';
+$_['column_component'] = 'Компонент';
+$_['column_action']    = 'Действие';
 
 // Entry
-$_['entry_theme']      = 'Theme';
+$_['entry_theme']      = 'Тема';
 $_['entry_sass']       = 'SASS';
-$_['entry_cache']      = 'Cache';
+$_['entry_cache']      = 'Кэш';
 
 // Button
-$_['button_on']        = 'On';
-$_['button_off']       = 'Off';
+$_['button_on']        = 'Вкл';
+$_['button_off']       = 'Выкл';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify developer settings!';
+$_['error_permission'] = 'Внимание: У Вас нет разрешения на изменение настроек разработчика!';
